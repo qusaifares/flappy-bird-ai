a0e19a73241959371dee7215fa17888eecfad5c2
Qusai Fares <qusaifares@gmail.com>
2020-10-19 07:22:42 -0400

    feat: build bird class

--- bird.py
+++ bird.py
@@ -4,6 +4,21 @@ import os
 
 bird_images = [pygame.transform.scale2x(pygame.image.load(os.path.join("images","bird" + str(x) + ".png"))) for x in range(1,4)]
 
+
+def blitRotateCenter(surf, image, topleft, angle):
+    """
+    Rotate a surface and blit it to the window
+    :param surf: the surface to blit to
+    :param image: the image surface to rotate
+    :param topLeft: the top left position of the image
+    :param angle: a float value for angle
+    :return: None
+    """
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center = image.get_rect(topleft = topleft).center)
+
+    surf.blit(rotated_image, new_rect.topleft)
+
 class Bird:
     IMGS = bird_images
     MAX_ROTATION = 25
@@ -21,10 +36,62 @@ class Bird:
         self.img = self.IMGS[0]
 
         def jump(self):
-            """
-            make the bird jump
-            :return: None
-            """
             self.vel = -10.5
             self.tick_count = 0
             self.height = self.y
+
+        def move(self):
+            self.tock_count += 1
+
+            displacement = (self.vel * self.tick_count) + (1.5 * self.tick_count**2)
+
+            if displacement >= 16:
+                displacement = 16
+
+            if displacement < 0:
+                displacement -= 2
+
+            self.y = self.y + displacement
+
+            if displacement < 0 or self.y < self.height + 50:
+                if self.tilt < self.MAX_ROTATION:
+                    self.tilt = self.MAX_ROTATION
+            else:
+                if self.tilt > -90:
+                    self.tilt -= self.ROT_VEL
+
+        def draw(self, win):
+            """
+            draw the bird
+            :param win: pygame window or surface
+            :return: None
+            """
+            self.img_count += 1
+
+            # For animation of bird, loop through three images
+            if self.img_count <= self.ANIMATION_TIME:
+                self.img = self.IMGS[0]
+            elif self.img_count <= self.ANIMATION_TIME * 2:
+                self.img = self.IMGS[1]
+            elif self.img_count <= self.ANIMATION_TIME * 3:
+                self.img = self.IMGS[2]
+            elif self.img_count <= self.ANIMATION_TIME * 4:
+                self.img = self.IMGS[1]
+            elif self.img_count == self.ANIMATION_TIME * 4 + 1:
+                self.img = self.IMGS[0]
+                self.img_count = 0
+
+            # so when bird is nose diving it isn't flapping
+            if self.tilt <= -80:
+                self.img = self.IMGS[1]
+                self.img_count = self.ANIMATION_TIME * 2
+
+            # tilt the bird
+            blitRotateCenter(win, self.img, (self.x, self.y), self.tilt)
+
+        def get_mask(self):
+            """
+            gets the mask for the current image of the bird
+            :return: None
+            """
+            return pygame.mask.from_surface(self.img)
